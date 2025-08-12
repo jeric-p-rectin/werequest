bef43a6c565633be0196bcdd65f551580227a0a1
Boboe16 <jerixmodz@gmail.com>
2025-08-12 23:38:17 +0800

    peee

--- app/components/Profile.tsx
+++ app/components/Profile.tsx
@@ -324,7 +324,7 @@ export default function Profile() {
     };
 
     if (isAdminUser && editable.password) {
-      (payload as string).password = editable.password;
+      (payload as any).password = editable.password;
     }
 
     const res = await fetch('/api/profile/edit-profile', {
